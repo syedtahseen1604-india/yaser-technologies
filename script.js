19cff1b0aacc8f55cf91c07f2886e8f03263a59d
syedtahseen1604-india <syedtahseen1604@gmail.com>
2026-09-12 19:06:02 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle');
-const nav = document.querySelector('.nav');
-
-menuToggle.addEventListener('click', () => nav.classList.toggle('open'));
-
-document.querySelectorAll('.nav a').forEach(link => {
-  link.addEventListener('click', () => nav.classList.remove('open'));
-});
